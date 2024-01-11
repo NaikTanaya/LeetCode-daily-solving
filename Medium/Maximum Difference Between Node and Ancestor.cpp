1f48d8c6e64ddb02be1962d2557e682ac5836c29
Tanaya Naik <78205020+NaikTanaya@users.noreply.github.com>
2024-01-11 22:34:27 +0530

    Maximum Difference Between Node and Ancestor.cpp

---
+++ Medium/Maximum Difference Between Node and Ancestor.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode* root) {
+        if (!root)
+            return 0;
+        int minVal = root->val, maxVal = root->val;
+        differ(root, minVal, maxVal);
+        return diff;
+    }
+private:
+    int diff = 0;
+
+    void differ(TreeNode* root, int minVal, int maxVal) {
+        if (!root)
+            return;
+        diff = max(diff, max(abs(minVal - root->val), abs(maxVal - root->val)));
+        minVal = min(minVal, root->val);
+        maxVal = max(maxVal, root->val);
+        differ(root->left, minVal, maxVal);
+        differ(root->right, minVal, maxVal);
+    }
+};
+
+// Example 1:
+
+// Input: root = [8,3,10,1,6,null,14,null,null,4,7,13]
+// Output: 7
+// Explanation: We have various ancestor-node differences, some of which are given below :
+// |8 - 3| = 5
+// |3 - 7| = 4
+// |8 - 1| = 7
+// |10 - 13| = 3
+// Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
+
+// Example 2:
+
+// Input: root = [1,null,2,null,0,3]
+// Output: 3
