5b2782985701cc039b5bec679cef44123688b96c
Tanaya Naik <78205020+NaikTanaya@users.noreply.github.com>
2024-02-15 21:03:15 +0530

    Find Polygon With the Largest Perimeter.cpp

---
+++ Medium/Find Polygon With the Largest Perimeter.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    long long largestPerimeter(vector<int>& nums) {
+        long long sum = 0;
+        sort(nums.begin(),nums.end());
+        for(auto i : nums) sum += i;
+        int n = nums.size();
+        for(int i = n - 1; i >= 2; i--){
+            sum -= nums[i];
+            if(sum > nums[i]){
+                return sum + nums[i];
+            }
+        }
+        return -1;
+    }
+};
+
+// Example 1:
+
+// Input: nums = [5,5,5]
+// Output: 15
+// Explanation: The only possible polygon that can be made from nums has 3 sides: 5, 5, and 5. The perimeter is 5 + 5 + 5 = 15.
+
+// Example 2:
+
+// Input: nums = [1,12,1,2,5,50,3]
+// Output: 12
+// Explanation: The polygon with the largest perimeter which can be made from nums has 5 sides: 1, 1, 2, 3, and 5. The perimeter is 1 + 1 + 2 + 3 + 5 = 12.
+// We cannot have a polygon with either 12 or 50 as the longest side because it is not possible to include 2 or more smaller sides that have a greater sum than either of them.
+// It can be shown that the largest possible perimeter is 12.
+
+// Example 3:
+
+// Input: nums = [5,5,50]
+// Output: -1
+// Explanation: There is no possible way to form a polygon from nums, as a polygon has at least 3 sides and 50 > 5 + 5.
