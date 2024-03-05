951299e88fa76977da15f22987ad833e0c0542bb
Tanaya Naik <78205020+NaikTanaya@users.noreply.github.com>
2024-03-05 23:06:50 +0530

    Minimum Length of String After Deleting Similar Ends.cpp

---
+++ Medium/Minimum Length of String After Deleting Similar Ends.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right && s[left] == s[right]) {
+            char ch = s[left];
+            while (left <= right && s[left] == ch) {
+                left++;
+            }
+            while (right >= left && s[right] == ch) {
+                right--;
+            }
+        }
+
+        return right - left + 1;
+    }
+};
+
+// Example 1:
+
+// Input: s = "ca"
+// Output: 2
+// Explanation: You can't remove any characters, so the string stays as is.
+
+// Example 2:
+
+// Input: s = "cabaabac"
+// Output: 0
+// Explanation: An optimal sequence of operations is:
+// - Take prefix = "c" and suffix = "c" and remove them, s = "abaaba".
+// - Take prefix = "a" and suffix = "a" and remove them, s = "baab".
+// - Take prefix = "b" and suffix = "b" and remove them, s = "aa".
+// - Take prefix = "a" and suffix = "a" and remove them, s = "".
+
+// Example 3:
+
+// Input: s = "aabccabba"
+// Output: 3
+// Explanation: An optimal sequence of operations is:
+// - Take prefix = "aa" and suffix = "a" and remove them, s = "bccabb".
+// - Take prefix = "b" and suffix = "bb" and remove them, s = "cca".
