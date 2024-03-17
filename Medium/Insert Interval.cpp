4925603a9819322dd35592a0e99a3e8eb627f4f5
Tanaya Naik <78205020+NaikTanaya@users.noreply.github.com>
2024-03-17 22:57:28 +0530

    Insert Interval.cpp

---
+++ Medium/Insert Interval.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> merged;
+        int i = 0;
+
+        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
+            merged.push_back(intervals[i]);
+            i++;
+        }
+
+        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+            newInterval = {min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])};
+            i++;
+        }
+        merged.push_back(newInterval);
+
+        while (i < intervals.size()) {
+            merged.push_back(intervals[i]);
+            i++;
+        }
+
+        return merged;
+    }
+};
+
+// Example 1:
+
+// Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+// Output: [[1,5],[6,9]]
+
+// Example 2:
+
+// Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+// Output: [[1,2],[3,10],[12,16]]
+// Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
