a95e93ad8ce3388298d6a7dacf0427ec9faf1ba6
Tanaya Naik <78205020+NaikTanaya@users.noreply.github.com>
2024-03-22 19:42:26 +0530

    Palindrome Linked List.cpp

---
+++ Easy/Palindrome Linked List.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> listVals;
+        while (head) {
+            listVals.push_back(head->val);
+            head = head->next;
+        }
+        
+        int left = 0, right = listVals.size() - 1;
+        while (left < right && listVals[left] == listVals[right]) {
+            left++;
+            right--;
+        }
+        return left >= right;
+    }
+};
+
+Example 1:
+
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+
+Input: head = [1,2]
+Output: false
